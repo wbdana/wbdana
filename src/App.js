c3d2c5530101fae6f397d616087098f350d7d1ed
William Dana <william.b.dana@gmail.com>
2019-02-13 23:56:50 -0500

    Build out skeleton Navbar and Footer; styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
 import './css/style.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar />
         <Header />
+        <Footer />
       </div>
     );
   }
