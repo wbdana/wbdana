9e64d7553a3a40faffe75ff87e044b625e939615
William Dana <william.b.dana@gmail.com>
2019-02-14 13:08:17 -0500

    Align Navbar items

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
   return (
     <div className="home">
       <Header />
-      <Navbar />
+      {/*<Navbar />*/}
       {/*<Footer />*/}
     </div>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,36 +2,36 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <div className="navbar">
     <div className="row">
+    <div className="navbar">
 
         <div className="col-1-of-4">
-          <div className="navbar__item">
-            <span className="navbar__item-text">
+          <div className="navbar__item navbar__item-1">
+            <span className="navbar__item navbar__item-text">
               Github
             </span>
           </div>
         </div>
 
         <div className="col-1-of-4">
-          <div className="navbar__item">
-            <span className="navbar__item-text">
+          <div className="navbar__item navbar__item-2">
+            <span className="navbar__item navbar__item-text">
               LinkedIn
             </span>
           </div>
         </div>
 
         <div className="col-1-of-4">
-          <div className="navbar__item">
-            <span className="navbar__item-text">
+          <div className="navbar__item navbar__item-3">
+            <span className="navbar__item navbar__item-text">
               Blog
             </span>
           </div>
         </div>
 
         <div className="col-1-of-4">
-          <div className="navbar__item">
-            <span className="navbar__item-text">
+          <div className="navbar__item navbar__item-4">
+            <span className="navbar__item navbar__item-text">
               Twitter
             </span>
           </div>
