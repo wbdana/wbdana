369c78d05a4106ba7d2dbeeec3a41334a0ce440e
William Dana <william.b.dana@gmail.com>
2019-02-14 11:49:57 -0500

    Style new navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,40 @@ import React from 'react';
 const Navbar = () => {
   return (
     <div className="navbar">
-      <div className="navbar__item">
-        <span className="navbar__item-text">
-          Github
-        </span>
-      </div>
+    <div className="row">
+
+        <div className="col-1-of-4">
+          <div className="navbar__item">
+            <span className="navbar__item-text">
+              Github
+            </span>
+          </div>
+        </div>
+
+        <div className="col-1-of-4">
+          <div className="navbar__item">
+            <span className="navbar__item-text">
+              LinkedIn
+            </span>
+          </div>
+        </div>
+
+        <div className="col-1-of-4">
+          <div className="navbar__item">
+            <span className="navbar__item-text">
+              Blog
+            </span>
+          </div>
+        </div>
+
+        <div className="col-1-of-4">
+          <div className="navbar__item">
+            <span className="navbar__item-text">
+              Twitter
+            </span>
+          </div>
+        </div>
+    </div>
     </div>
   );
 };
