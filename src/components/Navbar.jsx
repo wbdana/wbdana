69ddbb287139a77ac45fff318e78835ffd03b25d
William Dana <william.b.dana@gmail.com>
2019-02-16 12:23:41 -0500

    Start work on mobile optimizations

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,40 +3,39 @@ import React from 'react';
 const Navbar = () => {
   return (
     <div className="row">
-    <div className="navbar">
-
-        <div className="col-1-of-4">
-          <div className="navbar__item navbar__item-2">
-            <a href="https://github.com/wbdana" target="_blank" rel="noopener noreferrer" className="navbar__item navbar__item-text">
-              GitHub
-            </a>
+      <div className="navbar">
+          <div className="col-1-of-4">
+            <div className="navbar__item navbar__item-2">
+              <a href="https://github.com/wbdana" target="_blank" rel="noopener noreferrer" className="navbar__item navbar__item-text">
+                GitHub
+              </a>
+            </div>
           </div>
-        </div>
 
-        <div className="col-1-of-4">
-          <div className="navbar__item navbar__item-1">
-            <a href="https://linkedin.com/in/wbdana" target="_blank" rel="noopener noreferrer" className="navbar__item navbar__item-text">
-              LinkedIn
-            </a>
+          <div className="col-1-of-4">
+            <div className="navbar__item navbar__item-1">
+              <a href="https://linkedin.com/in/wbdana" target="_blank" rel="noopener noreferrer" className="navbar__item navbar__item-text">
+                LinkedIn
+              </a>
+            </div>
           </div>
-        </div>
 
-        <div className="col-1-of-4">
-          <div className="navbar__item navbar__item-3">
-            <a href="https://medium.com/@wbdana" target="_blank" rel="noopener noreferrer" className="navbar__item navbar__item-text">
-              Blog
-            </a>
+          <div className="col-1-of-4">
+            <div className="navbar__item navbar__item-3">
+              <a href="https://medium.com/@wbdana" target="_blank" rel="noopener noreferrer" className="navbar__item navbar__item-text">
+                Blog
+              </a>
+            </div>
           </div>
-        </div>
 
-        <div className="col-1-of-4">
-          <div className="navbar__item navbar__item-4">
-            <a href="https://twitter.com/_wbdana" target="_blank" rel="noopener noreferrer" className="navbar__item navbar__item-text">
-              Twitter
-            </a>
+          <div className="col-1-of-4">
+            <div className="navbar__item navbar__item-4">
+              <a href="https://twitter.com/_wbdana" target="_blank" rel="noopener noreferrer" className="navbar__item navbar__item-text">
+                Twitter
+              </a>
+            </div>
           </div>
-        </div>
-    </div>
+      </div>
     </div>
   );
 };
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,44 +2,81 @@ import React from 'react';
 import ProjectCard from './ProjectCard';
 
 const Projects = () => {
+  const projects = [
+    {
+      img: "https://i.imgur.com/SzsSGDt.jpg",
+      position: 1,
+      title: "Asteroids Online",
+      url: "https://asteroids-online.herokuapp.com",
+    },
+    {
+      img: "https://i.imgur.com/7jG8L5r.png",
+      position: 2,
+      title: "Connected Text",
+      url: "https://conntext.herokuapp.com",
+    },
+    {
+      img: "http://i.imgur.com/MubF8Tb.png",
+      position: 3,
+      title: "QSmart",
+      url: "https://qsmart-react.herokuapp.com",
+    },
+    {
+      img: "http://i.imgur.com/UUo9URs.png",
+      position: 4,
+      title: "Pixel Playground",
+      url: "https://pixel-playground.herokuapp.com/",
+    },
+  ];
+
   return (
     <section className="section-projects">
       <h2 className="header-projects">
         Projects
       </h2>
       <div className="row u-grid-max-width">
-        <div className="col-1-of-4">
-          <ProjectCard
-            img={"https://i.imgur.com/SzsSGDt.jpg"}
-            position={1}
-            title={"Asteroids Online"}
-            url={"https://asteroids-online.herokuapp.com"}
-          />
-        </div>
-        <div className="col-1-of-4">
-          <ProjectCard
-            img={"https://i.imgur.com/7jG8L5r.png"}
-            position={2}
-            title={"Connected Text"}
-            url={"https://conntext.herokuapp.com"}
-          />
-        </div>
-        <div className="col-1-of-4">
-          <ProjectCard
-            img={"http://i.imgur.com/MubF8Tb.png"}
-            position={3}
-            title={"QSmart"}
-            url={"https://qsmart-react.herokuapp.com/"}
-          />
-        </div>
-        <div className="col-1-of-4">
-          <ProjectCard
-            img={"http://i.imgur.com/UUo9URs.png"}
-            position={4}
-            title={"Pixel Playground"}
-            url={"https://pixel-playground.herokuapp.com/"}
-          />
-        </div>
+        {projects.map(project => (
+          <div className="col-1-of-4 resp">
+            <ProjectCard
+              img={project.img}
+              position={project.position}
+              title={project.title}
+              url={project.url}
+            />
+          </div>
+        ))}
+        {/*<div className="col-1-of-4">*/}
+          {/*<ProjectCard*/}
+            {/*img={"https://i.imgur.com/SzsSGDt.jpg"}*/}
+            {/*position={1}*/}
+            {/*title={"Asteroids Online"}*/}
+            {/*url={"https://asteroids-online.herokuapp.com"}*/}
+          {/*/>*/}
+        {/*</div>*/}
+        {/*<div className="col-1-of-4">*/}
+          {/*<ProjectCard*/}
+            {/*img={"https://i.imgur.com/7jG8L5r.png"}*/}
+            {/*position={2}*/}
+            {/*title={"Connected Text"}*/}
+            {/*url={"https://conntext.herokuapp.com"}*/}
+          {/*/>*/}
+        {/*</div>*/}
+        {/*<div className="col-1-of-4">*/}
+          {/*<ProjectCard*/}
+            {/*img={"http://i.imgur.com/MubF8Tb.png"}*/}
+            {/*position={3}*/}
+            {/*title={"QSmart"}*/}
+            {/*url={"https://qsmart-react.herokuapp.com/"}*/}
+          {/*/>*/}
+        {/*</div>*/}
+        {/*<div className="col-1-of-4">*/}
+          {/*<ProjectCard*/}
+            {/*img={"http://i.imgur.com/UUo9URs.png"}*/}
+            {/*position={4}*/}
+            {/*title={"Pixel Playground"}*/}
+            {/*url={"https://pixel-playground.herokuapp.com/"}*/}
+          {/*/>*/}
+        {/*</div>*/}
       </div>
     </section>
   );
